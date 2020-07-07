350d75e763ce9210a34f86ae48eb854b46d766c2
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-07 05:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n){
+  const listItems = document.querySelectorAll('.ranked-list li')
+
+  for (let i in listItems){
+    listItems[i].innerHTML += parseInt(n)
+  }
+}
