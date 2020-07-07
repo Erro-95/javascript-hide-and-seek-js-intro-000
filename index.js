710aa2d09e159652f9a3081755fddfb608d0dff4
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-07 06:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,8 +31,3 @@ function deepestChild(){
     }
   }
 }
-
-
-Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node.
-(Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them. This is challenging
-to implement correctly, but not beyond your ability!)
