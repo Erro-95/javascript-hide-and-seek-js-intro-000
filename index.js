b2281e49304099cc4eaf9ba875e518d0f519bb01
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-07 06:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function increaseRankBy(n){
     listItems[i].innerHTML = `${parseString + n}`
   }
 }
+
+function deepestChild(){
+  let current = document.querySelector('div#grand-node')
+  let next;
+
+  while (current){
+    if (current.innerHTML){
+      return current
+    }
+
+    else {
+      next = current
+      current = next.querySelector('div')
+    }
+  }
+}
+
+
+Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node. 
+(Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them. This is challenging 
+to implement correctly, but not beyond your ability!)
